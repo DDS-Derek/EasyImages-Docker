72622c27d6ab0ee4a9a756e8e79110b361bf15cb
DDSRem <1448139087@qq.com>
2023-02-01 13:12:31 +0800

    Optimize log output & configuration file creation

--- shell/020-install.sh
+++ shell/020-install.sh
@@ -1,56 +1,79 @@
 #!/bin/bash
 
-# 创建配置文件
 if [ ! -f "/app/web/config/config.php" ]; then
- echo -e "\033[36m config.php文件不存在,正在创建... \033[0m"
- mv /config/config.php /app/web/config
- echo "config.php文件创建成功"
+    echo -e "\033[36m config.php文件不存在,正在创建... \033[0m"
+    cp /config/config.php /app/web/config/config.php
+    if [ -f "/app/web/config/config.php" ]; then
+        echo "config.php文件创建成功"
+    else
+        echo "config.php文件创建失败"
+        exit 1
+    fi
 else
- echo "config.php文件存在"
- rm -rf /config/config.php
+    echo "config.php文件存在"
 fi
 
 if [ ! -f "/app/web/config/api_key.php" ]; then
- echo -e "\033[36m api_key.php文件不存在,正在创建... \033[0m"
- mv /config/api_key.php /app/web/config
- echo "api_key.php文件创建成功"
+    echo -e "\033[36m api_key.php文件不存在,正在创建... \033[0m"
+    cp /config/api_key.php /app/web/config/api_key.php
+    if [ -f "/app/web/config/api_key.php" ]; then
+        echo "api_key.php文件创建成功"
+    else 
+        echo "api_key.php文件创建失败"
+        exit 1
+    fi
 else
- echo "api_key.php文件存在"
- rm -rf /config/api_key.php
+    echo "api_key.php文件存在"
 fi
 
 if [ ! -f "/app/web/config/config.guest.php" ]; then
- echo -e "\033[36m config.guest.php文件不存在,正在创建... \033[0m"
- mv /config/config.guest.php /app/web/config
- echo "config.guest.php文件创建成功"
+    echo -e "\033[36m config.guest.php文件不存在,正在创建... \033[0m"
+    cp /config/config.guest.php /app/web/config/config.guest.php
+    if [ -f "/app/web/config/config.guest.php" ]; then
+        echo "config.guest.php文件创建成功"
+    else
+        echo "config.guest.php文件创建失败"
+        exit 1
+    fi
 else
- echo "config.guest.php文件存在"
- rm -rf /config/config.guest.php
+    echo "config.guest.php文件存在"
 fi
 
 if [ ! -f "/app/web/config/config.manager.php" ]; then
- echo -e "\033[36m config.manager.php文件不存在,正在创建... \033[0m"
- mv /config/config.manager.php /app/web/config
- echo "config.manager.php文件创建成功"
+    echo -e "\033[36m config.manager.php文件不存在,正在创建... \033[0m"
+    cp /config/config.manager.php /app/web/config/config.manager.php
+    if [ -f "/app/web/config/config.manager.php" ]; then
+        echo "config.manager.php文件创建成功"
+    else
+        echo "config.manager.php文件创建失败"
+        exit 1
+    fi
 else
- echo "config.manager.php文件存在"
- rm -rf /config/config.manager.php
+    echo "config.manager.php文件存在"
 fi
 
 if [ ! -f "/app/web/i/.htaccess" ]; then
- echo -e "\033[36m .htaccess文件不存在,正在创建... \033[0m"
- mv /i/.htaccess /app/web/i
- echo ".htaccess文件创建成功"
+    echo -e "\033[36m .htaccess文件不存在,正在创建... \033[0m"
+    cp /i/.htaccess /app/web/i/.htaccess
+    if [ -f "/app/web/i/.htaccess" ]; then
+        echo ".htaccess文件创建成功"
+    else
+        echo ".htaccess文件创建失败"
+        exit 1
+    fi
 else
- echo ".htaccess文件存在"
- rm -rf /i/.htaccess
+    echo ".htaccess文件存在"
 fi
 
 if [ ! -f "/app/web/i/index.html" ]; then
- echo -e "\033[36m index.html文件不存在,正在创建... \033[0m"
- mv /i/index.html /app/web/i
- echo "index.html文件创建成功"
+    echo -e "\033[36m index.html文件不存在,正在创建... \033[0m"
+    cp /i/index.html /app/web/i/index.html
+    if [ -f "/app/web/i/index.html" ]; then
+        echo "index.html文件创建成功"
+    else
+        echo "index.html文件创建失败"
+        exit 1
+    fi
 else
- echo "index.html文件存在"
- rm -rf /i/index.html
+    echo "index.html文件存在"
 fi
